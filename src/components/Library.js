512cd6e487a356e7f41bdd1d3defaa4e0dedbe85
felicia <felicia@hyperarts.com>
2018-03-24 14:32:36 -0700

    Checkpoint 2: Routing completed (correction2).

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Library = () => (
-    <section className="landing">Library will go here.</section>
+    <section className="library">Library will go here.</section>
 );
 
 export default Library;
