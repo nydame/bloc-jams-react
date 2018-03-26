62df7b7a62cb649426555a9345b04d8d6ae4bafa
felicia <felicia@hyperarts.com>
2018-03-25 21:27:34 -0700

    Resubmission: Get rid of syntax error.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,22 +4,22 @@ const Landing = () => (
     <section className="landing">
         <h1 className="hero-title">Turn the music up!</h1>
         <section className="selling-points">
-            <div class="point">
-                <h2 class="point-title">Choose your music.</h2>
-                <p class="point-description">
+            <div className="point">
+                <h2 className="point-title">Choose your music.</h2>
+                <p className="point-description">
                     The world is full of music. Why should you have to listen to
                     music chosen by someone else?
                 </p>
             </div>
-            <div class="point">
-                <h2 class="point-title">Unlimited, streaming, ad-free</h2>
-                <p class="point-description">
+            <div className="point">
+                <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+                <p className="point-description">
                     No arbitrary limits. No distractions.
                 </p>
             </div>
-            <div class="point">
-                <h2 class="point-title">Mobile enabled.</h2>
-                <p class="point-description">
+            <div className="point">
+                <h2 className="point-title">Mobile enabled.</h2>
+                <p className="point-description">
                     Listen to your music on the go. This streaming service is
                     available on all mobile platforms.
                 </p>
