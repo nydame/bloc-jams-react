c60d9b9b75dfb5db7a8e9aeb0f111167904bee58
felicia <felicia@hyperarts.com>
2018-04-13 05:48:37 -0700

    Add axios Get element to Library to handle request to Spotify API (not yet functional).

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 import './../styles/Library.css';
+import { AxiosProvider, Request, Head, Get, withAxios } from 'react-axios';
 
 class Library extends Component {
     constructor(props) {
@@ -40,6 +41,27 @@ class Library extends Component {
                     </Link>
                 ))}
                 <aside className="chooser">
+                    <Get url="https://accounts.spotify.com/authorize?client_id=ae45a76e6e264417a49a32043284912b&response_type=token&redirect_uri=https:%2F%2Fnydame-bloc-jams-v2.netlify.com%2Flibrary%2F&state=12345">
+                        {(error, response, isLoading, onReload) => {
+                            if (error) {
+                                return (
+                                    <div>
+                                        Oops! Spotify could not be loaded due to{' '}
+                                        {error.message}
+                                    </div>
+                                );
+                            } else if (isLoading) {
+                                return <div>{'Loading...'}</div>;
+                            } else if (response !== null) {
+                                return <div>{response.data.message} </div>;
+                            }
+                            return (
+                                <div>
+                                    Default message before request is made.
+                                </div>
+                            );
+                        }}
+                    </Get>
                     <button
                         className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
                         onClick={() => this.handleButtonClick()}
