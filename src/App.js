7abd6eb51e866def6e9debd35fda4e5c8696b4c7
felicia <felicia@hyperarts.com>
2018-10-14 15:41:43 -0700

    Install Firebase and connect to it from App component.

--- src/App.js
+++ src/App.js
@@ -4,34 +4,45 @@ import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 import './App.css';
+import { database } from './data/firebase';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header>
-                    <h1>
-                        <NavLink to="/" exact>
-                            <img src="/assets/images/bloc_jams_logo.png" />
-                        </NavLink>
-                    </h1>
-                    <nav>
-                        <NavLink to="/" exact>
-                            Home
-                        </NavLink>
-                        <NavLink to="/library" exact>
-                            Library
-                        </NavLink>
-                    </nav>
-                </header>
-                <main>
-                    <Route exact path="/" component={Landing} />
-                    <Route path="/library" component={Library} />
-                    <Route path="/album/:slug" component={Album} />
-                </main>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      stats: null,
+      user: null,
+    };
+  }
+
+  componentDidMount() {console.log(database);}
+
+  render() {
+    return (
+        <div className="App">
+            <header>
+                <h1>
+                    <NavLink to="/" exact>
+                        <img src="/assets/images/bloc_jams_logo.png" />
+                    </NavLink>
+                </h1>
+                <nav>
+                    <NavLink to="/" exact>
+                        Home
+                    </NavLink>
+                    <NavLink to="/library" exact>
+                        Library
+                    </NavLink>
+                </nav>
+            </header>
+            <main>
+                <Route exact path="/" component={Landing} />
+                <Route path="/library" component={Library} />
+                <Route path="/album/:slug" component={Album} />
+            </main>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyApKX5t4q4wcz5LPp77noqk9x_fw8xJrlQ",
+  authDomain: "blocmetrics-4e5b3.firebaseapp.com",
+  databaseURL: "https://blocmetrics-4e5b3.firebaseio.com",
+  projectId: "blocmetrics-4e5b3",
+  storageBucket: "blocmetrics-4e5b3.appspot.com",
+  messagingSenderId: "576734756421"
+};
+firebase.initializeApp(config);
+
+export default firebase;
+export const database = firebase.database();
