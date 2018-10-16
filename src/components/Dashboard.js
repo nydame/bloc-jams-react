b497c7753bb42f8e6929e957ca65d7c254e790d8
felicia <felicia@hyperarts.com>
2018-10-16 11:35:14 -0700

    Display album selection data on the Dashboard page.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,11 +7,21 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <section className="dashboard">
+      <section className="dashboard--summary">
         <h1>Welcome to the Dashboard!</h1>
         <h2>Page Views</h2>
         <p>Home Page: {this.props.stats.homeViews} unique visits</p>
         <p>Library Page: {this.props.stats.libraryViews} unique visits</p>
+        <h2>Album Popularity</h2>
+        <div>
+          {Object.entries(this.props.stats.albumSelections).map(
+            (entry, index) => (
+              <p key={index}>
+                {entry[0]}: selected {entry[1]} times
+              </p>
+            )
+          )}
+        </div>
       </section>
     );
   }
